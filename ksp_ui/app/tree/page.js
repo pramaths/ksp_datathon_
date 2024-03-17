c8b277a5ede4037656f38503d94d85c359b02da4
pramaths <103276292+pramaths@users.noreply.github.com>
2024-03-18 03:14:09 +0530

    something

--- ksp_ui/app/tree/page.js
+++ ksp_ui/app/tree/page.js
@@ -1,10 +1,12 @@
 
 import Tree from "../../components/tree";
+const TreeComponentNoSSR = dynamic(() => import('../../components/tree'), {
+    ssr: false, // This line is key to avoiding SSR for the map component
+  });
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-
-   <Tree/>
+<TreeComponentNoSSR/>
     </main>
   );
 }
