137f317075dd3857570f2595ff8985fdd914430f
pramaths <103276292+pramaths@users.noreply.github.com>
2024-03-18 03:08:47 +0530

    something

--- ksp_ui/app/page.js
+++ ksp_ui/app/page.js
@@ -3,7 +3,10 @@ import Homepage from '../components/Home'
 import Bar from '../components/Bar'
 import BubbleChart from "@/components/BubbleChart";
 import Tree from "../components/tree";
-import DistributedTree from '../components/DistributesTree'
+// import DistributedTree from '../components/DistributesTree'
+const DistributedTree = dynamic(() => import('../components/DistributesTree'), {
+  ssr: false, // This line is key to avoiding SSR for the map component
+});
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
